b6ac47317bf45401831e67351c25f3dcf678c001
nickr <nickro@ya.ru>
2021-05-19 14:23:59 +0300

    rename shops choice form

--- webapp/parse/forms.py
+++ webapp/parse/forms.py
@@ -6,20 +6,15 @@ from wtforms import widgets, SubmitField, HiddenField, SelectMultipleField
 from webapp.model import db, Shop
 
 
-class ParseForm(FlaskForm):
-    shops = ['one', 'two']
-    submit = SubmitField('Parse', render_kw={"class": "btn btn-primary"})
-
-
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
 
 
-class SimpleForm(FlaskForm):
-    list_of_files = ['citilink', 'regard']
-    print(f'list_of_files: {list_of_files}, {type(list_of_files)}')
+class ShopsChoiceForm(FlaskForm):
+    list_of_shops = ['citilink', 'regard']
+    print(f'list_of_files: {list_of_shops}, {type(list_of_shops)}')
     # create a list of value/description tuples
-    shops = [(x, x) for x in list_of_files]
+    shops = [(x, x) for x in list_of_shops]
     print(f'files: {shops}')
     example = MultiCheckboxField('Label', choices=shops)
